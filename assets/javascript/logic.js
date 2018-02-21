335026fdf371c48e0bbcfa641b62936ad78f6558
Dakota <drakoden2000@gmail.com>
2018-02-21 09:52:21 -0800

    new changes

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,8 +2,10 @@ var animals = ["cats", "dogs", "birds"]
 
 
 
-
+// This calls the data from the API
 function displayAnimalGifs() {
+    $("#giphy-view").empty();  
+
     var animal = $(this).attr("data-name");
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?" + "api_key=c0C2JonSEtMJVQMWbDj0Y3qUMe2D1Q4F" + "&q=" + animal + "&limit=10"
@@ -15,21 +17,26 @@ function displayAnimalGifs() {
     }).then(function (response) {
         $("#giphy-view").append('<div id="giphyJson">')
         console.log(response);
-        console.log(response.data[0].images.fixed_height_still.height)
-        var imgTag = $('<img>')
-        $(imgTag).addClass('gif');
-        $("#giphyJson").append(imgTag);
-        var stillImg = response.data[0].images.fixed_height_still.url
-        var animateImg = response.data[0].images.fixed_height.url
-        $(imgTag).attr('data-state', 'still');
-        $(imgTag).attr('src', stillImg);
-        $(imgTag).attr('data-still', stillImg);
-        $(imgTag).attr('data-animate', animateImg);
+        for (i = 0; i < 10; i++) {
+            var imgTag = $('<img>')
+            $(imgTag).addClass('gif');
+            $("#giphyJson").append(imgTag);
+            var stillImg = response.data[i].images.fixed_height_still.url
+            var animateImg = response.data[i].images.fixed_height.url
+            $(imgTag).attr('data-state', 'still');
+            $(imgTag).attr('src', stillImg);
+            $(imgTag).attr('data-still', stillImg);
+            $(imgTag).attr('data-animate', animateImg);
+            var ratingDiv = $("<div>");
+            $("#giphyJson").append(ratingDiv);
+            $(ratingDiv).append("Rating: " + response.data[i].rating);
+        }
     })
-    
+
 }
 
-$("#add-animal").on('click', function(event) {
+// This adds animals to the buttons already established
+$("#add-animal").on('click', function (event) {
     event.preventDefault();
     var animal = $("#animal-input").val().trim();
 
@@ -37,6 +44,7 @@ $("#add-animal").on('click', function(event) {
     renderButtons();
 })
 
+// This puts the buttons on the page. Isn't called yet
 function renderButtons() {
     $("#buttons-view").empty();
 
@@ -48,13 +56,15 @@ function renderButtons() {
         button.text(animals[i]);
         $("#buttons-view").append(button);
     }
-    
-}
 
+}
+// When a button is clicked, calls the data from the named function
 $(document).on("click", ".animal", displayAnimalGifs);
+
 renderButtons();
 
-$(".gif").on("click", function() {
+// This format allows elements created after the page load to still be clicked
+$(document).on("click", ".gif", function () {
     var state = $(this).attr('data-state')
 
     if (state === 'still') {
